a89228b4d38c6f0b706aaf8ada7f33a6e54dea9a
Devin Alexander <alexanderdev117@gmail.com>
2020-01-16 13:57:44 -0600

    server.js added, database sql files added.

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burger_DB;
+CREATE database burger_DB;
+
+USE burger_DB;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(30) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT false,
+  PRIMARY KEY (id)
+);
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'kaiju0790'
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Classic');
+INSERT INTO burgers (burger_name) VALUES ('Double with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chili Burger');
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
