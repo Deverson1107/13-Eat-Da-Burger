00f8f2c2c993b1809611a51c9439e1d5bf3b08aa
Devin Alexander <alexanderdev117@gmail.com>
2020-01-19 16:20:48 -0600

    another attempt

--- db/schema.sql
+++ db/schema.sql
@@ -6,9 +6,10 @@ USE burger_DB;
 
 
 CREATE TABLE burgers (
-  id INT NOT NULL AUTO_INCREMENT,
-  burger_name VARCHAR(30) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(30),
   devoured BOOLEAN DEFAULT false,
+  createdAt TIMESTAMP NOT NULL,
   PRIMARY KEY (id)
 );
 
